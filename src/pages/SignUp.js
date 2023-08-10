9cf2f6f4c2740840545fc9564047c01094dd72aa
강경규 <qwzx16@naver.com>
2023-08-11 00:25:53 +0900

    test

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -16,7 +16,7 @@ export default function SignUp() {
   return (
     <>
       <div className="container p-5">
-        <h1 className="text-center">로고</h1>
+        <h1 className="text-center">로고릴라</h1>
       </div>
       <div className="container">
         <form className="col-6 m-auto form-min-w">
